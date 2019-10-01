ce78ee5013fa4c430dec68fcee68356c3776f80e
Brian Junker-Latocha <b.k.junker@gmail.com>
2019-10-01 22:35:59 +0200

    exercise 2

---
+++ Ex02/code.js
@@ -0,0 +1,15 @@
+// This is a comment, everything after is ignored
+
+console.log("I am Code");
+
+/* THis is also a comment
+* you use this for longer things.
+*/
+
+console.log("So am I");
+
+//THe code inside the next comment will not run
+
+/*
+console.log("I will not run");
+*/
