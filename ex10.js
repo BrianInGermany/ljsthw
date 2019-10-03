b6c9af69d402fa6b35abf0187d955e8ceaa3be35
Brian Junker-Latocha <b.k.junker@gmail.com>
2019-10-03 11:43:56 +0200

    added user input to ex10

--- ex10.js
+++ ex10.js
@@ -1,9 +1,21 @@
 // Exercise 10: Files, args, variables
-
+const readline = require('readline-sync');
 const fs = require('fs');
 
 let file_to_open = process.argv[2];
 let file_contents = fs.readFileSync(file_to_open);
 
+
+console.log(`This is argv[0]: ${process.argv[0]}.`);
 console.log(`The file named ${file_to_open} contains:`);
 console.log(file_contents.toString());
+
+let name = readline.question("name an additional text file to open: \n");
+if (fs.existsSync(name)) {
+    let file_two_contents = fs.readFileSync(name);
+    console.log(`The file named ${name} contains:`);
+    console.log(file_two_contents.toString());
+}
+else {
+    console.log("No such file!")
+}
