b44c2caa861176001761c5e0232388c513529750
Brian Junker-Latocha <b.k.junker@gmail.com>
2019-10-03 11:35:52 +0200

    combining argv, readfile, format strings

---
+++ ex10.js
@@ -0,0 +1,9 @@
+// Exercise 10: Files, args, variables
+
+const fs = require('fs');
+
+let file_to_open = process.argv[2];
+let file_contents = fs.readFileSync(file_to_open);
+
+console.log(`The file named ${file_to_open} contains:`);
+console.log(file_contents.toString());
